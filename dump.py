e9df333419c5abc2b4b7eb3c51bcd8607caca078
alfonsf1 <figueroa.a@csu.fullerton.edu>
2021-03-11 19:34:01 -0800

    Uploading finished dump.py

--- dump.py
+++ dump.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python3
+import sys
 import shelve
+import requests
+
+firstArgv = sys.argv[1]
+resp = requests.get(str(firstArgv))
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError("URL doesn't exist!")
+
+list_of_keys = resp.json()['keys']
+
 
 with shelve.open('./var/kv.dbm') as db:
-    for k in db.keys():
-        print(f'{{\'{k}\': \'{db[k]}\'}}')
+    for key in list_of_keys:
+        print(f'{{\'{key}\': \'{db[key]}\'}}')
 
 
+#commands to run
+# chmod 755 dump.py 
+#./dump.py http://localhost:5100
