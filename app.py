439a0a513a16a96b7ea84c2041e0ce0e2d8a1326
alfonsf1 <figueroa.a@csu.fullerton.edu>
2021-03-13 20:11:52 -0800

    finished adding session func and deleting session func

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import bottle
 from bottle import get, post, request, response, template, redirect
 import requests
 import uuid
+import shelve
 
 
 # Set up app and logging
@@ -35,43 +36,83 @@ def show_form():
     # response = session.get('http://localhost:5000')
     sessionID1 = request.cookies.get("count1")
     sessionID2 = request.cookies.get('count2')
-    print(sessionID1)
-    print(sessionID2)
-    if not sessionID1:
-        sessionID1 = str(uuid.uuid4())
+    # print(sessionID1)
+    # print(sessionID2)
+    # if not sessionID1:
+    #     sessionID1 = str(uuid.uuid4())
+    #     response.set_cookie('count1', str(sessionID1))
 
-    if not sessionID2:
-        sessionID2 = str(uuid.uuid4())
+    # if not sessionID2:
+    #     sessionID2 = str(uuid.uuid4())
+    #     response.set_cookie('count2', str(sessionID2))
 
     print("session1: ",sessionID1)
-    print("session2: ", sessionID2)
-    
-    count1 = request.get_cookie('count1', default=str(sessionID1))
-    count2 = request.get_cookie('count2', default=str(sessionID2))
-    print(count1)
+    # print("session2: ", sessionID2)
+
+    with shelve.open('./var/kv.dbm') as db: 
+        if not sessionID1:
+            sessionID1 = str(uuid.uuid4())
+            response.set_cookie('count1', sessionID1)
+            db[sessionID1] = 0
+            sessionID2 = str(uuid.uuid4())
+            response.set_cookie('count2', sessionID2)
+            db[sessionID2] = 0
+        
+        count1 = db[sessionID1]
+        count2 = db[sessionID2]
+        
+        count1 += 1
+        db[sessionID1] = int(count1)
+        return template('counter.tpl', counter1=count1, counter2=count2)
+
+
+    # count1 = request.get_cookie('count1', default=str(sessionID1))
+    # count2 = request.get_cookie('count2', default=str(sessionID2))
+    # print(count1)
+
+
 
     # count1 = int(count1) + 1
 
-    response.set_cookie('count1', str(count1))
+    # response.set_cookie('count1', str(count1))
 
-    return template('counter.tpl', counter1=count1, counter2=count2)
 
 @post('/increment')
 def increment_count2():
     # session = requests.Session()
     # response = session.get('http://localhost:5000')
     #print(response)
-    sessionID = str(uuid.uuid4())
-    count2 = request.get_cookie('count2', default=sessionID)
+    # checkSessionID = response.get_cookie('count2')
+    # sessionID = str(uuid.uuid4())
+    # count2 = request.get_cookie('count2', default=sessionID)
     # count2 = int(count2) + 1
-    response.set_cookie('count2', str(count2))
+    sessionID = request.get_cookie('count2')
+    with shelve.open('./var/kv.dbm') as db:
+        count2 = db[sessionID]
 
-    return redirect('/')
+        count2 += 1
+
+        db[sessionID] = int(count2)
+        return redirect('/')
+
+    # response.set_cookie('count2', str(count2))
+
 
 
 @post('/reset')
 def reset_counts():
-    response.delete_cookie('count1')
-    response.delete_cookie('count2')
 
-    return redirect('/')
+    with shelve.open('./var/kv.dbm') as db:
+        getSessionID1 = request.get_cookie('count1')
+        getSessionID2 = request.get_cookie('count2')
+
+        db.pop(getSessionID1, None)
+        db.pop(getSessionID2, None)
+        response.delete_cookie('count1')
+        response.delete_cookie('count2')
+
+        return redirect('/')
+
+
+# a0125de5-8b5c-47e8-972e-c98d05b8c932
+# "6205d68b-bfa4-451f-9932-9dd28b5e6abe"
